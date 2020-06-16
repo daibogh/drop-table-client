f15813ca22419d6a9e3b31f522b3a6fffb6abd5b
daibogh <daibogh@gmail.com>
2020-06-16 11:40:00 +0300

    removed comments from razzle config

--- razzle.config.js
+++ razzle.config.js
@@ -6,76 +6,12 @@ const postcssNormalize = require('postcss-normalize');
 const ForkTSCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const {addSassPlugin} = require('./config/sass-plugin')
 
-// const sassRegex = /\.(scss|sass)$/;
-// const sassModuleRegex = /\.module\.(scss|sass)$/;
-// const getStyleLoaders = (cssOptions, preProcessor) => {
-// 	const loaders = [
-// 		{
-// 			loader: MiniCssExtractPlugin.loader
-// 		},
-// 		{
-// 			loader: require.resolve('css-loader'),
-// 			options: cssOptions,
-// 		},
-// 		{
-// 			loader: require.resolve('postcss-loader'),
-// 			options: {
-// 				ident: 'postcss',
-// 				plugins: () => [
-// 					require('postcss-flexbugs-fixes'),
-// 					require('postcss-preset-env')({
-// 						autoprefixer: {
-// 							flexbox: 'no-2009',
-// 						},
-// 						stage: 3,
-// 					}),
-// 					postcssNormalize(),
-// 				],
-// 				sourceMap: false,
-// 			},
-// 		},
-// 	].filter(Boolean);
-// 	if (preProcessor) {
-// 		loaders.push({
-// 			loader: require.resolve(preProcessor),
-// 			options: {
-// 				sourceMap: false,
-// 			},
-// 		});
-// 	}
-// 	return loaders;
-// };
-
 module.exports = {
   plugins: [
     'babel-ts',
-  //   {
-  //   name: 'typescript',
-  //   // options: {
-  //   //   useBabel: true,
-  //   //   tsLoader: {
-  //   //     transpileOnly: true,
-  //   //     experimentalWatchApi: true,
-  //   //   },
-  //   //   forkTsChecker: {
-  //   //     tsconfig: './tsconfig.json',
-  //   //     tslint: './tslint.json',
-  //   //     watch: './src',
-  //   //     typeCheck: true,
-  //   //   },
-  //   // },
-  //   options: {
-  //     useBabel: true,
-  //     useEslint: true,
-  //     forkTsChecker: {
-  //       tsconfig: './tsconfig.json',
-  //       tslint: undefined,
-  //       watch: './src',
-  //       typeCheck: true
-  //     }
-  //   }
-  // },
-'bundle-analyzer', 'serviceworker'],
+    'bundle-analyzer',
+    'serviceworker'
+  ],
   modify: (defaultConfig, { target, dev }, webpack) => {
 
     let config = defaultConfig;
@@ -96,42 +32,6 @@ module.exports = {
       );
     }
 		config = addSassPlugin(config)
-    // config.plugins.push(new MiniCssExtractPlugin({
-		// 	filename: 'static/css/bundle.[contenthash:8].css',
-		// 	chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
-		// 	allChunks: true,
-
-		// }))
-
-		// config.module.rules.push(
-		// 	{
-		// 		test: sassRegex,
-		// 		exclude: sassModuleRegex,
-		// 		use: getStyleLoaders(
-		// 			{
-		// 				importLoaders: 2,
-		// 				sourceMap: false,
-		// 			},
-		// 			'sass-loader'
-		// 		),
-		// 		sideEffects: true,
-		// 	},
-		// 	{
-		// 		test: sassModuleRegex,
-		// 		use: getStyleLoaders(
-		// 			{
-		// 				importLoaders: 2,
-		// 				sourceMap: false,
-		// 				modules: true,
-		// 				getLocalIdent: getCSSModuleLocalIdent,
-		// 			},
-		// 			'sass-loader'
-		// 		),
-		// 	}
-		// )
-    
-
-    
     return config;
   },
 };
