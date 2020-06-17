0bd0c3521f96733ffac2872b9922821102062c92
daibogh <daibogh@gmail.com>
2020-06-17 21:10:32 +0300

    .env

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+// check this https://create-react-app.dev/docs/advanced-configuration/
+// and this https://github.com/jaredpalmer/razzle/tree/master/examples/with-custom-environment-variables
+
+export const apiUrl = process.env.RAZZLE_API_URL
