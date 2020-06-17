4178d67dfc4a55b7e30f1544687bd1548af36bc7
daibogh <daibogh@gmail.com>
2020-06-17 23:31:30 +0300

    fix names, added procfile

--- src/api/fetchers.ts
+++ src/api/fetchers.ts
@@ -1,6 +1,7 @@
 import ky from 'ky'
-import {apiUrl} from '~/constants'
+import { apiUrl } from '~/constants'
 
-export const fetcher = ky.create({prefixUrl: apiUrl})
+export const fetcher = ky.create({ prefixUrl: apiUrl })
 export const getFetcher = async <T>(url: string) => fetcher.get(url).json<T>()
-export const postFetcher = async <T,K>(url: string, options:T) => fetcher.post(url,options).json<K>()
+export const postFetcher = async <T, K>(url: string, options: T) =>
+  fetcher.post(url, options).json<K>()
