b9feb6f162cd4dc57745b3dc87c37d8c84e20e1f
daibogh <daibogh@gmail.com>
2020-06-20 18:15:48 +0300

    feat: added react-use, updated toggle

--- src/app/Toggle/Toggle.tsx
+++ src/app/Toggle/Toggle.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import classnames from 'classnames'
 import { Button } from 'shared/base/Button';
 import './Toggle.scss';
+import { useToggle } from 'react-use';
 
 interface ToggleProps {
   className?: string;
+  on: boolean,
+  toggle: () => void
 }
 
-export const Toggle: React.FC<ToggleProps> = ({ className }) => {
+export const Toggle: React.FC<ToggleProps> = ({ className, on, toggle }) => {
   return (
     <div className={`Toggle ${className}`}>
-      <div className="btn-group" role="group">
-        <Button className="LeftButton btn btn-primary">Список</Button>
-        <Button className="RightButton btn btn-outline-primary">Граф</Button>
+      <div className="btn-group" role="group" onClick={toggle}>
+        <Button className={classnames('LeftButton', 'btn', { 'btn-primary': on, },)}>Список</Button>
+        <Button className={classnames('RightButton', 'btn', { 'btn-primary': !on, })}>Граф</Button>
       </div>
     </div>
   );
