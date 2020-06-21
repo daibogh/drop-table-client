aef5683679f0406f3a2fa7d87fc025b3cf408c10
Anastasia Kogteva <52398853+anastasia-kogteva@users.noreply.github.com>
2020-06-21 05:39:11 +0300

    add_putch

--- src/data/programs/model.ts
+++ src/data/programs/model.ts
@@ -1,63 +1,72 @@
 export interface Program {
-    name: string
-    description?: string
-    hours?: number
-    category?: string
-    disciplines: Discipline[]
-    created_at?: string
-    deleted_at?: string
-    is_deleted?: boolean
-    rating?: number
-    id: number
-    parameters: Parameter[]
-  }
-  
-  export interface Discipline { id?: number; name: string; category: string }
-  
-  export interface Parameter{
-    id?: number
-    name?: string
-    weight: number
-    value?: string
-  }
-  export interface NewProgram {
-    name: string
-    description?: string
-    hours?: number
-    category?: string
-    disciplines: number[]
-    created_at?: string
-    deleted_at?: string
-    parameters?:Parameter[]
-  }
-  
-  export interface Error{
-    ok:boolean;
-    error_code:string;
-    error:  HTTPValidationError;
-  }
-  export interface HTTPValidationError {
-    detail?: ValidationError[]
-  }
-  
-  export interface ValidationError{ loc: string[]; msg: string; type: string }
+  name: string;
+  description?: string;
+  hours?: number;
+  category?: string;
+  disciplines: Discipline[];
+  created_at?: string;
+  deleted_at?: string;
+  is_deleted?: boolean;
+  rating?: number;
+  id?: number;
+  parameters: Parameter[];
+}
 
-  export interface MiniDiscipline {
-    id: number;
-    name: string;
-  }
+export interface Discipline {
+  id?: number;
+  name: string;
+  category: string;
+}
 
-  export interface YearDiff {
-    added: MiniDiscipline[],
-    removed: MiniDiscipline[]
-  }
+export interface Parameter {
+  id?: number;
+  name?: string;
+  weight: number;
+  value?: string;
+}
+export interface NewProgram {
+  name: string;
+  description?: string;
+  hours?: number;
+  category?: string;
+  disciplines: number[];
+  created_at?: string;
+  deleted_at?: string;
+  parameters?: Parameter[];
+  id?: number;
+}
 
-  export interface YearStats {
-    diff: YearDiff,
-    rating?: 0
-  }
+export interface Error {
+  ok: boolean;
+  error_code: string;
+  error: HTTPValidationError;
+}
+export interface HTTPValidationError {
+  detail?: ValidationError[];
+}
 
-  export interface Stats {
-    ok: boolean;
-    data: {[key: number]: YearStats}
-  }
+export interface ValidationError {
+  loc: string[];
+  msg: string;
+  type: string;
+}
+
+export interface MiniDiscipline {
+  id: number;
+  name: string;
+}
+
+export interface YearDiff {
+  added: MiniDiscipline[];
+  removed: MiniDiscipline[];
+}
+
+export interface YearStats {
+  diff: YearDiff;
+  rating?: 0;
+}
+
+export interface Stats {
+  ok: boolean;
+  data: { [key: number]: YearStats };
+}
